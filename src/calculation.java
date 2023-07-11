b19a53ac238f28e56e278d5ff024b6546bc1b918
Mason-Zeng <masonzeng123@gmail.com>
2023-07-10 23:27:43 -0400

    Calculation part of add and subtract method works. Need to work on rounding

--- src/calculation.java
+++ src/calculation.java
@@ -1,10 +1,103 @@
 public class calculation {
-    private double answer;
-    private String num1;
-    private String num2;
-
+    private double answer; private String num1;private String num2;private double val_1;private double val_2;private boolean decimal;
     public calculation(String n1, String n2){
         this.num1 = n1;
         this.num2 = n2;
+        this.val_1 = Double.parseDouble(num1);
+        this.val_2 = Double.parseDouble(num2);
+        if(!n1.contains(".") && !n2.contains(".")){
+            this.decimal = false;
+            //System.out.println("false");
+        }
+        else{
+            this.decimal = true;
+            //System.out.println("true");
+        }
+    }
+    private int numDecimal(String n){
+        int amount = 0;
+        boolean flag = false;
+        for(int i =0; i<n.length(); i++){
+            if(n.substring(i,i+1).equals(".")){
+                flag = true;
+            }
+            if(flag){
+                amount++;
+            }
+        }
+        amount-=1;
+        return amount;
+    }
+    public String add(){
+        this.answer = this.val_1 + this.val_2;
+        if(decimal == false){
+            return Double.toString(this.answer);
+        }
+        int num_d1=0; int num_d2=0; int d;
+        num_d1 = numDecimal(this.num1);
+        //System.out.println(num_d1);
+        num_d2 = numDecimal(this.num2);
+        //System.out.println(num_d2);
+        if(num_d1 <= num_d2){
+            d = num_d1;
+        }
+        else{
+            d = num_d2;
+        }
+        int curr_d = numDecimal(Double.toString(this.answer));
+        //next step compare curr_d and d
+        String original = Double.toString(this.answer);
+        if(curr_d <= d){
+            int diff = d-curr_d;
+            for(int i=0; i<diff;i++){
+                original = original + "0";
+            }
+            return original;
+        }
+        String temp = "";
+        for(int j=0; j<original.length();j++){
+            temp += original.substring(j,j+1);
+            if(original.substring(j,j+1).equals(".")){
+                temp += original.substring(j+1, j+d+1);
+                break;
+            }
+        }
+        return temp;
+    }
+    public String subtract(){
+        this.answer = this.val_1 - this.val_2;
+        if(decimal == false){
+            return Double.toString(this.answer);
+        }
+        int num_d1=0; int num_d2=0; int d;
+        num_d1 = numDecimal(this.num1);
+        //System.out.println(num_d1);
+        num_d2 = numDecimal(this.num2);
+        //System.out.println(num_d2);
+        if(num_d1 <= num_d2){
+            d = num_d1;
+        }
+        else{
+            d = num_d2;
+        }
+        int curr_d = numDecimal(Double.toString(this.answer));
+        //next step compare curr_d and d
+        String original = Double.toString(this.answer);
+        if(curr_d <= d){
+            int diff = d-curr_d;
+            for(int i=0; i<diff;i++){
+                original = original + "0";
+            }
+            return original;
+        }
+        String temp = "";
+        for(int j=0; j<original.length();j++){
+            temp += original.substring(j,j+1);
+            if(original.substring(j,j+1).equals(".")){
+                temp += original.substring(j+1, j+d+1);
+                break;
+            }
+        }
+        return temp;
     }
 }
--- src/calculator.java
+++ src/calculator.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class calculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+       /*
         System.out.println("Put in a number to calculate the # of sig figs");
         String n= scan.nextLine();
         numbers test = new numbers(n);
@@ -9,5 +10,12 @@ public class calculator {
         test.trailZero();
         test.numSig();
         System.out.println(test);
+        */
+        System.out.println("Put two numbers to add");
+        String x = scan.nextLine();
+        String y = scan.nextLine();
+        System.out.println(Double.parseDouble(x) - Double.parseDouble(y));
+        calculation calc = new calculation(x,y);
+        System.out.println(calc.subtract());
     }
 }
